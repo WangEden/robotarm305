711d6eaa81d8a6c74678a8f6d9029be6561d2253
WangEden <wangeden_@outlook.com>
2024-12-27 13:07:53 +0800

    first commit

---
+++ build/robotarm305/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/eden/Project/catkin_ws/src/robotarm305
+# Build directory: /home/eden/Project/catkin_ws/build/robotarm305
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
